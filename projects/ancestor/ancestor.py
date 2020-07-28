f13c902af4b7db0db38a51ff4ae138f6098a538f
jackskim <jackskim29@gmail.com>
2020-07-28 06:02:51 -0700

    Cleanup

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -53,7 +53,6 @@ class Graph:
         for parent in parents:
             self.dft_ancestors(parent[0], paths, longest, path)
 
-
 def earliest_ancestor(ancestors, starting_node):
     # Create a graph
     g = Graph()
@@ -102,10 +101,6 @@ def earliest_ancestor(ancestors, starting_node):
         # Otherwise, return the last ancestor in the path
         return paths[0][-1]
 
-test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+#test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
-print(earliest_ancestor(test_ancestors, 3)) # should print 10
-
-
-
-
+#print(earliest_ancestor(test_ancestors, 3)) # should print 10
